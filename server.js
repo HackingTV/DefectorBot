61e48dbcf1f4c7bc0f1a6c1e533c8d7133afdf7c
Christo Mitov <christo.mitov@gmail.com>
2018-05-25 22:02:00 -0400

    suppart for subs

--- server.js
+++ server.js
@@ -68,15 +68,15 @@ app.get('/defectors', async (req, res) => {
   res.json(await api.getDefectors())
 })
 
-app.get('/testFollow/:name', (req, res) => {
-  io.emit('follow', req.params.name)
-  res.sendStatus(200)
-})
+// app.get('/testFollow/:name', (req, res) => {
+//   io.emit('follow', req.params.name)
+//   res.sendStatus(200)
+// })
 
-app.get('/testSubscribe/:name', (req, res) => {
-  io.emit('subscribe', req.params.name)
-  res.sendStatus(200)
-})
+// app.get('/testSubscribe/:name', (req, res) => {
+//   io.emit('subscribe', req.params.name)
+//   res.sendStatus(200)
+// })
 
 app.get('/widgets/alert', (req, res) => {
   res.sendFile(path.join(__dirname, './widgets/alert', 'index.html'))
