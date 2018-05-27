5b192ef002a1076f763727c0a755e3c2975b81f7
Christo Mitov <christo.mitov@gmail.com>
2018-05-26 23:30:09 -0400

    pls

--- bots/twitchbot.js
+++ bots/twitchbot.js
@@ -2,6 +2,7 @@ const tmi = require('twitch-js')
 const api = require('../api')
 const logger = require('../logger')
 const io = require('../server').io
+const trivia = require('../trivia')
 
 const channel = 'hackingtv'
 
@@ -17,7 +18,7 @@ const options = {
 }
 
 const client = new tmi.client(options)
-console.log(io)
+
 const handleSubscription = async (channel, username, method, message, userstate) => {
   console.log(`subscription detected, firing lights ${username}`)
   logger.info(`subscription detected, firing lights ${username}`)
@@ -40,6 +41,12 @@ client.on('chat', async (channel, userstate, message, self) => {
   // Don't listen to my own messages..
   if (self) return
 
+  if (trivia.isPlaying()) {
+    if (trivia.isAnswer(message)) {
+      await client.say(channel, 'CORRECT!')
+    }
+  }
+
   if(message === '!defectors') {
     try {
       let defectors = await api.getDefectorsFromDB()
@@ -52,6 +59,10 @@ client.on('chat', async (channel, userstate, message, self) => {
       console.error(err)
       logger.error(err)
     }
+  } else if (message === '!trivia' && !trivia.isPlaying()) {
+    //return client.say(channel, 'Starting a game of trivia!')
+    let question = await trivia.getQuestion()
+    await client.say(channel, question)
   } else if (message === '!help') {
     await client.say(channel, 'Use !defectors to list the defectors')
   }
---
+++ trivia/index.js
@@ -0,0 +1,49 @@
+const axios = require('axios')
+
+const API_URL = 'http://jservice.io'
+
+let state = {
+  CURRENT_QUESTION: {},
+  IS_PLAYING: false,
+  TIMEOUT: null
+}
+
+const getRandomQuestionAPI = () => (
+  axios.get(API_URL + '/api/random')
+    .then(res => res.data[0])
+)
+
+const getQuestion = async () => {
+  let question = await getRandomQuestionAPI()
+  question.answer = (question.answer).replace(/[\W_]+/g,'')
+
+  state.CURRENT_QUESTION = question
+  state.IS_PLAYING = true
+  state.TIMEOUT = setTimeout(() => {
+    // Nobody ansnwered the question at this point so.. gg
+    state.IS_PLAYING = false
+  }, 1000 * 60)
+
+  console.log(state.CURRENT_QUESTION)
+  return state.CURRENT_QUESTION.question
+}
+
+const isAnswer = answer => {
+  if (answer.toLowerCase() === state.CURRENT_QUESTION.answer.toLowerCase()) {
+    state.IS_PLAYING = false
+    clearTimeout(state.TIMEOUT)
+    return true
+  }
+
+  return false
+}
+
+const isPlaying = () => {
+  return state.IS_PLAYING
+}
+
+module.exports = {
+  getQuestion,
+  isAnswer,
+  isPlaying
+}
